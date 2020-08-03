44f0b305fa6b279c1ee984b85cad5775a7fe63c4
Phuongduc123 <56790882+Phuongduc123@users.noreply.github.com>
2020-08-03 15:11:57 +0700

    commit today oke

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./styles.css";
-import _ from 'lodash';
+import _ from "lodash";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>
-        {_.upperCase('Hello gjhgjhg CodeSandbox')}</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>{_.upperCase("Hello gjhgjhg CodeSandbox")}</h1>
+      <h2>Start jhgjhgj editing to see some magic happen!</h2>
     </div>
   );
 }
