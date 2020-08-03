df54b69557d7b631bac402d5fd7b368c6086456f
Phuongduc123 <56790882+Phuongduc123@users.noreply.github.com>
2020-08-03 15:08:53 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import _ from 'lodash';
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        {_.upperCase('Hello gjhgjhg CodeSandbox')}</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
